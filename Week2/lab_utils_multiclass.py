b74625c6e5b630277abc28fd1776e4b897eed7eb
Madara Wimalarathna <33604605+madarasw@users.noreply.github.com>
2025-08-01 23:57:17 +0530

    Add files via upload
    deeplearning.mplstyle
    lab_utils_common.py
    lab_utils_multiclass.py
    lab_utils_multiclass_TF.py
    lab_utils_relu.py
    lab_utils_softmax.py
    autils.py

---
+++ Week2/lab_utils_multiclass.py
@@ -0,0 +1,62 @@
+# ⚠️ Note: This file is modified for use in Google Colab with Google Drive.
+# Upload this file directly to the appropriate folder in your Google Drive.
+# Make sure to update any file paths accordingly to match your Drive structure.
+
+##########################################################
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+import sys
+sys.path.append("/content/drive/MyDrive/Colab Notebooks")
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('/content/drive/MyDrive/Colab Notebooks/Course 2/Week 2/deeplearning.mplstyle')
+
+##########################################################
+
+# C2_W1 Utilities
+from sklearn.datasets import make_blobs
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Plot  multi-class training points
+def plot_mc_data(X, y, class_labels=None, legend=False,size=40):
+    classes = np.unique(y)
+    for i in classes:
+        label = class_labels[i] if class_labels else "class {}".format(i)
+        idx = np.where(y == i)
+        plt.scatter(X[idx, 0], X[idx, 1],  cmap=plt.cm.Paired,
+                    edgecolor='black', s=size, label=label)
+    if legend: plt.legend()
+        
+
+#Plot a multi-class categorical decision boundary
+# This version handles a non-vector prediction (adds a for-loop over points)
+def plot_cat_decision_boundary(X,predict , class_labels=None, legend=False, vector=True):
+
+    # create a mesh to points to plot
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    h = max(x_max-x_min, y_max-y_min)/200
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    points = np.c_[xx.ravel(), yy.ravel()]
+    print("points", points.shape)
+    print("xx.shape", xx.shape)
+
+    #make predictions for each point in mesh
+    if vector:
+        Z = predict(points)
+    else:
+        Z = np.zeros((len(points),))
+        for i in range(len(points)):
+            Z[i] = predict(points[i].reshape(1,2))
+    Z = Z.reshape(xx.shape)
+
+    #contour plot highlights boundaries between values - classes in this case
+    plt.figure()
+    plt.contour(xx, yy, Z, colors='g') 
+    plt.axis('tight')
